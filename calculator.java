da9863adf27d964a63e6439961f0fe48a9769760
U10416027 <U10416027@go.utaipei.edu.tw>
2016-06-06 00:15:30 +0800

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+
+public class calculator extends Application {
+  public void start(Stage primaryStage) {
+  
+  }
+}
