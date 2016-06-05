45ce1c7dc2d6ffd2006095b371079cb917c38bbd
U10416027 <U10416027@go.utaipei.edu.tw>
2016-06-06 00:14:33 +0800

    Create TestCalculator.java

---
+++ TestCalculator.java
@@ -0,0 +1,10 @@
+//U10416027  董郡麟
+import javafx.application.Application;
+
+public class TestCalculator{
+  public static void main(String[] args) {
+		
+    Application.launch(calculator.class, args);
+		
+  }
+}
